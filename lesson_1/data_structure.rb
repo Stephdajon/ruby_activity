d52e67fa3f3c5ef7cc9d9d3ebf80647bb8ef1c0a
stephdajon <stephdajon@gmail.com>
2021-08-03 20:34:13 +0800

    first assignment done

---
+++ lesson_1/data_structure.rb
@@ -0,0 +1,19 @@
+contact_data = [["ana@email.com", "123 Main st.", "555-123-4567"], ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Analyn Cajocson" => {}, "Avion School" => {}}
+
+contacts["Analyn Cajocson"][:email] = conta
+ct_data[0][0]
+contacts["Analyn Cajocson"][:address] = con
+tact_data[0][1]
+contacts["Analyn Cajocson"][:phone] = conta
+ct_data[0][2]
+
+contacts["Avion School"][:email] = contact_
+data[1][0]
+contacts["Avion School"][:address] = contac
+t_data[1][1]
+contacts["Avion School"][:phone] = contact_
+data[1][2]
+
+puts contacts
---
+++ lesson_1/iterate_array.rb
@@ -0,0 +1,3 @@
+num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+num_array.each { |n| puts n}
